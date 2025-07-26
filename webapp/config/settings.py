ec80e3d63fece5fb67f563ffa92015410dca20db
Kristian Kolthoff <k_kolthoff@web.de>
2025-07-26 15:00:45 +0200

    Update settings.py

--- webapp/config/settings.py
+++ webapp/config/settings.py
@@ -130,8 +130,9 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             'hosts': [(
-                os.environ.get('REDIS_HOST', '127.0.0.1'),
-                int(os.environ.get('REDIS_PORT', 6379))
+                "redis", 6379,
+                #os.environ.get('REDIS_HOST', '127.0.0.1'),
+               # int(os.environ.get('REDIS_PORT', 6379))
             )],
         },
     },
