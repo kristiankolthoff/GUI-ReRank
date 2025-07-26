727f1f73790203f9fdc3a0604a105123c8312f18
Kristian Kolthoff <k_kolthoff@web.de>
2025-07-26 15:00:20 +0200

    Update import dataset script

--- webapp/datasets/management/commands/import_dataset.py
+++ webapp/datasets/management/commands/import_dataset.py
@@ -10,18 +10,20 @@ from pathlib import Path
 from django.conf import settings
 from gui_rerank.annotator.config import DEFAULT_SEARCH_DIMENSIONS
 from datasets.models import SearchDimension as DjangoSearchDimension
+from tqdm import tqdm
 
 class Command(BaseCommand):
     help = "Import a merged RICO dataset into the Django database and create ImageEntry objects. Provide a dataset folder with --folder and a dataset name with --name."
 
     def add_arguments(self, parser):
-        parser.add_argument('--folder', type=str, required=True, help='Folder name of the dataset to import (e.g., rico_filtered_merged)')
+        parser.add_argument('--dataset_path', type=str, required=True, help='Folder name of the dataset to import)')
         parser.add_argument('--name', type=str, required=True, help='Name for the imported dataset in the database')
+        parser.add_argument('--images_path', type=str, required=True, help='Directory containing the source images')
 
     def handle(self, *args, **options):
         # 1. Load the framework dataset
         dataset_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../gui_rerank/resources/datasets/'))
-        dataset_name = options['folder']
+        dataset_name = options['dataset_path']
         framework_dataset = FrameworkDataset.load(dataset_dir, name=dataset_name)
         print(framework_dataset)
         # 2. Create Django Dataset object
@@ -33,6 +35,7 @@ class Command(BaseCommand):
             state="success",
         )
         django_dataset.save()
+        print(f"Created Django Dataset object with id {django_dataset.id}")
         # 2.1. Create SearchDimension objects for this dataset using DEFAULT_SEARCH_DIMENSIONS
         for dim in DEFAULT_SEARCH_DIMENSIONS:
             DjangoSearchDimension.objects.create(
@@ -46,6 +49,7 @@ class Command(BaseCommand):
                 pos_weight=dim.pos_weight,
                 neg_weight=dim.neg_weight
             )
+        print(f"Created {len(DEFAULT_SEARCH_DIMENSIONS)} SearchDimension objects")
         # 3. Create ImageEntry objects and use ImageEntry.id as index
         new_annotations = {}
         new_index_mapping = {}
@@ -63,17 +67,10 @@ class Command(BaseCommand):
             rico_to_imageentry[rico_id] = image_entry.id
             new_annotations[image_entry.id] = annotation
             new_inverse_index_mapping[image_entry.id] = framework_dataset.inverse_index_mapping[rico_id]
+        print(f"Created {len(rico_to_imageentry)} ImageEntry objects")
         for key, value in framework_dataset.index_mapping.items():
             new_index_mapping[key] = rico_to_imageentry[value]
-
-        # Save new files or update DB as needed
-        # Example: save new annotation/index mapping as JSON files
-        output_dir = os.path.join(dataset_dir, dataset_name)
-        with open(os.path.join(output_dir, 'annotations_db.json'), 'w', encoding='utf-8') as f:
-            json.dump(new_annotations, f, indent=2, ensure_ascii=False)
-        with open(os.path.join(output_dir, 'index_mapping_db.json'), 'w', encoding='utf-8') as f:
-            json.dump(new_index_mapping, f, indent=2, ensure_ascii=False)
-
+        print(f"Created {len(new_index_mapping)} index mapping")
         # 5. Save all dataset files to /media/dataset_dataset/<dataset_id>/ using the Dataset.save method
         dataset_media_dir_base = Path(settings.MEDIA_ROOT) / 'dataset_dataset'
         dataset_media_dir = dataset_media_dir_base / str(django_dataset.id)
@@ -84,13 +81,15 @@ class Command(BaseCommand):
         framework_dataset.inverse_index_mapping = new_inverse_index_mapping
         framework_dataset.name = str(django_dataset.id)
         framework_dataset.save(base_path=str(dataset_media_dir_base))
-
+        print(f"Copied framework dataset")
         # 6. Copy each included RICO image to /media/dataset_images/<dataset_id>/
-        source_image_dir = Path(r"E:/ZBook Juli 2025/Data/workspace_python/GUI2R/webapp/gui2rapp/staticfiles/resources/combined")
+        source_image_dir = Path(options['images_path'])
         images_media_dir = Path(settings.MEDIA_ROOT) / 'dataset_images' / str(django_dataset.id)
         images_media_dir.mkdir(parents=True, exist_ok=True)
         num_copied = 0
-        for rico_id, imageentry_id in rico_to_imageentry.items():
+        image_ids = list(rico_to_imageentry.keys())
+        for rico_id in tqdm(image_ids, desc="Copying images", unit="img"):
+            imageentry_id = rico_to_imageentry[rico_id]
             src_img = source_image_dir / f"{rico_id}.jpg"
             dst_img = images_media_dir / f"{rico_id}.jpg"
             if src_img.exists():
